24f21131af29257678443c959c4a1679e6bf2258
azimov <azimov@kolesa.kz>
2022-02-27 14:42:54 +0600

    module3-task1: feature add player page

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import mainPageProps from './state/main-page-props';
 import App from './components/app/app';
 import Login from './pages/login/login';
 import NotFound from './pages/not-found/not-found';
-import mainPageProps from './state/main-page-props';
 import MyList from './pages/my-list/my-list';
 import Movie from './pages/movie/movie';
 import AddReview from './pages/add-review/add-review';
+import Player from './pages/player';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,7 +21,7 @@ ReactDOM.render(
           <Route path=":id" element={<Movie/>} />
           <Route path=":id/review" element={<AddReview/>} />
         </Route>
-        {/*<Route path="player/:id" />*/}
+        <Route path="player/:id" element={<Player/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
