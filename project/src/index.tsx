16dcb8e9eba5961951b993970facee2c8cc5ec5d
azimov <azimov@kolesa.kz>
2022-02-27 14:37:07 +0600

    module3-task1: feature add routing for pages

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import App from './components/app/app';
+import Login from './pages/login/login';
+import NotFound from './pages/not-found/not-found';
 import mainPageProps from './state/main-page-props';
+import MyList from './pages/my-list/my-list';
+import Movie from './pages/movie/movie';
+import AddReview from './pages/add-review/add-review';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App {...mainPageProps} />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App {...mainPageProps} />} />
+        <Route path="login" element={<Login/>} />
+        <Route path="mylist" element={<MyList/>} />
+        <Route path="films">
+          <Route path=":id" element={<Movie/>} />
+          <Route path=":id/review" element={<AddReview/>} />
+        </Route>
+        {/*<Route path="player/:id" />*/}
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'));
