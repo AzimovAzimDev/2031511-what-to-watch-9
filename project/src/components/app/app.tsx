2236203ce1fde3344416d6a970778c69a84658d1
azimov <azimov@kolesa.kz>
2022-02-27 19:53:06 +0600

    module3-task1: refactoring move routers in app

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,30 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import mainPageProps from '../../state/main-page-props';
+import routes from '../../routes/routes';
 import Home from '../../pages/home/home';
+import Login from '../../pages/login/login';
+import MyList from '../../pages/my-list/my-list';
+import Movie from '../../pages/movie/movie';
+import AddReview from '../../pages/add-review/add-review';
+import Player from '../../pages/player';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private/private';
 import {MoviePageProps} from '../../types/MoviePage';
+
 function App(props: MoviePageProps): JSX.Element {
   return (
-    <Home { ...props } />);
+    <Routes>
+      <Route index element={<Home {...mainPageProps} />} />
+      <Route path={routes.login.path} element={<Login/>} />
+      <Route path={routes.myList.path} element={<PrivateRoute><MyList/></PrivateRoute>} />
+      <Route path={routes.movie.name} >
+        <Route path={routes.movie.path} element={<Movie/>} />
+        <Route path={routes.review.path} element={<AddReview/>} />
+      </Route>
+      <Route path={routes.player.path} element={<Player/>} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>);
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import routes from './routes/routes';
+import {BrowserRouter} from 'react-router-dom';
 import mainPageProps from './state/main-page-props';
 import App from './components/app/app';
-import Login from './pages/login/login';
-import NotFound from './pages/not-found/not-found';
-import MyList from './pages/my-list/my-list';
-import Movie from './pages/movie/movie';
-import AddReview from './pages/add-review/add-review';
-import Player from './pages/player';
-import PrivateRoute from './components/private/private';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App {...mainPageProps} />} />
-        <Route path={routes.login.path} element={<Login/>} />
-        <Route path={routes.myList.path} element={<PrivateRoute><MyList/></PrivateRoute>} />
-        <Route path={routes.movie.name} >
-          <Route path={routes.movie.path} element={<Movie/>} />
-          <Route path={routes.review.path} element={<AddReview/>} />
-        </Route>
-        <Route path={routes.player.path} element={<Player/>} />
-        <Route path="*" element={<NotFound/>} />
-      </Routes>
+      <App {...mainPageProps}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'));
