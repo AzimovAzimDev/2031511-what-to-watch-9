8da61dabdc980771532105686840509f22edcbef
azimov <azimov@kolesa.kz>
2022-02-28 21:33:49 +0600

    module4-task1: feature натсройка реактивности в форме

--- project/src/components/add-review-form/add-review-form.tsx
+++ project/src/components/add-review-form/add-review-form.tsx
@@ -1,22 +1,55 @@
-import {useState} from 'react';
+import { FormEventHandler, useState} from 'react';
 
 /**
  * Maximum review points
  */
 const MAX_POINTS = 10;
 
+/**
+ * Компонента для получения отзыва по фильму
+ */
 export default function AddReviewForm() {
 
-  const [points, setPoints] = useState(Array(MAX_POINTS).fill(false));
+  const [points, setPoints] = useState(Array(MAX_POINTS).fill(true));
+  const [review, setReview] = useState('');
+
+  /**
+   * Обработчик события на изменения рейтинга
+   * @param index - индек звезды на который кликнули
+   */
   const handleChange = (index: number) => {
     const position = (MAX_POINTS - index) ;
     const newAr = [...Array(MAX_POINTS - position).fill(false), ...Array(position).fill(true)];
     setPoints(newAr);
   };
 
+  /**
+   * Обработчик события на ввод в поле описания
+   * @param event
+   */
+  const handleTextAreaInput: FormEventHandler<HTMLTextAreaElement> = (event) => {
+    setReview((event.target as HTMLInputElement).value);
+  };
+
+  /**
+   * Получения рейтинга в цифре который указал пользователь
+   */
+  const getPoints = (): number => MAX_POINTS - points.lastIndexOf(false) - 1;
+
+  /**
+   * Обработчик события на отправку формы
+   */
+  const handleSubmit = () => {
+    // eslint-disable-next-line no-console
+    console.log({
+      points: getPoints(),
+      review,
+    });
+  };
+
   return(
     <div className="add-review">
-      <form action="#" className="add-review__form">
+      <form action="#" className="add-review__form" onSubmit={(e) => e.preventDefault()}>
         <div className="rating">
           <div className="rating__stars">
             {
@@ -34,7 +67,7 @@ export default function AddReviewForm() {
                       name="rating"
                       type="checkbox"
                       className="rating__input"
-                      onClick={() => handleChange(index)}
+                      onChange={() => handleChange(index)}
                     />
                     <label
                       key={labelKey}
@@ -52,12 +85,23 @@ export default function AddReviewForm() {
         </div>
 
         <div className="add-review__text">
-          <textarea className="add-review__textarea" name="review-text" id="review-text"
+          <textarea
+            value={review}
+            id="review-text"
+            name="review-text"
             placeholder="Review text"
+            className="add-review__textarea"
+            onInput={handleTextAreaInput}
           >
           </textarea>
           <div className="add-review__submit">
-            <button className="add-review__btn" type="submit">Post</button>
+            <button
+              type="submit"
+              className="add-review__btn"
+              onClick={handleSubmit}
+            >
+              Post
+            </button>
           </div>
 
         </div>
